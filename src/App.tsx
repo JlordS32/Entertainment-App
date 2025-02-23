f97f36c1c2a537fc97d06f23451e96ad6ac27699
JlordS32 <jlordsty128@gmail.com>
2025-02-24 06:36:26 +1100

    finished search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchDiscoverMovies } from "./api/fetchMovies";
+import Search from "./components/Search";
 
 const App = () => {
    const [data, setData] = useState<any>();
@@ -15,13 +16,10 @@ const App = () => {
    }, [data]);
 
    return (
-      <div className="bg-accent p-10 font-black text-white">
-         <h1>Entertain</h1>
-         <h2>Entertain</h2>
-         <h3>Entertain</h3>
-         <h4>Entertain</h4>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, delectus error? Placeat delectus fugit iste blanditiis cupiditate doloribus ipsum. Suscipit soluta aspernatur autem architecto exercitationem. Provident nulla expedita libero officia minus nisi omnis illo vero, earum incidunt itaque deleniti cum ea rerum ipsum consequuntur ratione quam! Amet suscipit temporibus architecto.</p>
-         <p className="text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A voluptatem repellat et! Esse porro exercitationem eum possimus nisi magni, alias necessitatibus fugit ullam voluptatibus, at rerum eos quis. Est, dolor?</p>
+      <div className="bg-primary p-8 font-black text-white">
+         <div className="w-[500px]">
+            <Search />
+         </div>
       </div>
    );
 };
---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+const Search = () => {
+   return (
+      <div className="search-container">
+         <img src="./images/search.svg" alt="Search Icon" />
+         <input className="search" type="text" placeholder="Search" />
+      </div>
+   );
+};
+
+export default Search;
